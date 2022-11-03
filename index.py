bf0488d935e9352cf541d4bb2c786cd6c1cff437
Gabriel Prates da Costa <gabriel.prates.costa@gmail.com>
2022-11-03 19:48:56 -0300

    Loop to click in connect button added

--- index.py
+++ index.py
@@ -13,10 +13,20 @@ sleep(2)
 #Confirm Title
 assert "LinkedIn" in driver.title
 
+#Wait login by user (input email and password, click login button and set "enter" in terminal)
 input()
 
+#Find element and click
 elem = driver.find_element(By.XPATH, '/html/body/div[5]/header/div/nav/ul/li[2]')
 elem.click()
 
+#Wait 4 seconds and scroll page
 sleep(4)
-driver.execute_script("window.scrollTo(0, 2000);")
+driver.execute_script("window.scrollTo(0, 2300);")
+
+#Wait 2 seconds and repeatedly click the "connect" button
+sleep(2)
+while(True):
+    elem = driver.find_element(By.XPATH, '//*[@id="ember482"]')
+    elem.click()
+    sleep(1)
